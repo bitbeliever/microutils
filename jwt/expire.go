81bc1324eb59d79ecb5d2541d6a4ba3eae593f45
shox <shoxhuang@gmail.com>
2022-04-24 22:18:52 +0800

    add jwt

---
+++ jwt/expire.go
@@ -0,0 +1,27 @@
+package jwt
+
+import (
+	"github.com/golang-jwt/jwt"
+	"time"
+)
+
+func IsExpire(tokenStr string) bool {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil {
+		return true
+	}
+	exp, ok := token.Header["exp"].(int64)
+	if !ok {
+		return true
+	}
+
+	expireAt := time.Unix(exp, 0)
+	if time.Now().After(expireAt) {
+		return true
+	}
+
+	return false
+}
